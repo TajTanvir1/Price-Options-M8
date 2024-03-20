ddffd738d37316de5a2a2227d4cf4b1791c5c9a7
tajtanvvir1 <tanvirah1223@gmail.com>
2024-03-20 23:02:07 +0600

    Daisy Nav added

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import Daisy from './components/Daisy'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
-      <div>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Ex nesciunt illum est, eum ab explicabo ipsum hic consectetur laborum. Officiis.</p>
-        <p>Voluptatem, adipisci labore laudantium officiis saepe cumque veniam, corrupti minus sequi voluptates sed, inventore dignissimos voluptate ducimus suscipit ullam ex!</p>
-        <p>Quaerat tempore dicta architecto repudiandae ad omnis facilis sed ducimus exercitationem. Placeat, non voluptas. Consequatur nostrum impedit iusto quam molestias.</p>
-        <p>Nesciunt ea inventore quos omnis, molestiae veritatis saepe voluptas libero laudantium quae tempore. Eum quam, culpa incidunt non corrupti dolore.</p>
-        <p>Quis molestias provident nostrum animi quibusdam molestiae error aliquam fugit saepe, vero ullam similique veritatis asperiores aut! Quia, fugiat cumque?</p>
-      </div>git add .
+      <Daisy></Daisy>
     </>
   )
 }
