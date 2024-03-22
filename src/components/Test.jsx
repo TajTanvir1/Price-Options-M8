e2809df6dd52ee8200ea4aa7287ff15c5875ac85
tajtanvvir1 <tanvirah1223@gmail.com>
2024-03-22 23:33:25 +0600

    22.03.24 Dom

--- src/components/Test.jsx
+++ src/components/Test.jsx
@@ -8,8 +8,16 @@ const Test = () => {
             <p>Porro rerum suscipit vel molestiae incidunt nisi voluptates ad voluptate quis! Debitis repudiandae repellendus dolorum temporibus libero eligendi itaque eos?</p>
             <p>Ducimus, quae nam sit blanditiis, dolor voluptatibus tempora ad optio enim iure beatae, dolorum velit ipsum neque minima vel eius?</p>
          </div>
+         <div>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Earum nostrum deleniti quos eius illum reprehenderit eligendi accusamus excepturi et perferendis?</p>
+            <p>Perspiciatis quas at odio id earum minima beatae, nemo, natus perferendis amet accusamus quisquam quo aliquid cum sequi, quibusdam mollitia!</p>
+            <p>Impedit, odit culpa cupiditate quas minima dolore. Id error quia dolore eaque doloribus! Minima autem quidem ex? Dolore, tempore facilis.</p>
+            <p>Porro rerum suscipit vel molestiae incidunt nisi voluptates ad voluptate quis! Debitis repudiandae repellendus dolorum temporibus libero eligendi itaque eos?</p>
+            <p>Ducimus, quae nam sit blanditiis, dolor voluptatibus tempora ad optio enim iure beatae, dolorum velit ipsum neque minima vel eius?</p>
+         </div>
       </div>
    );
 };
 
 export default Test;
+
